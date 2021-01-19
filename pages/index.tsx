75514f3490168ec017539f5221ae59f1d9e6b1dc
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-19 11:06:36 -0500

    typescript setup (yea boiiiii)

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: React.FC = () => (
+	<div className={styles.container}>
+		<Head>
+			<title>Blog App</title>
+		</Head>
+	</div>
+);
+
+export default Home;
