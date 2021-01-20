e6fc73c26d206874a76eb116b181a1a0e32a8af5
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-19 19:02:51 -0500

    arrow functions go brr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,68 +19,66 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
 	};
 };
 
-const Home: React.FC<Props> = ({ recentPosts }) => {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Next.js Blog</title>
-			</Head>
+const Home: React.FC<Props> = ({ recentPosts }) => (
+	<div className={styles.container}>
+		<Head>
+			<title>Next.js Blog</title>
+		</Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>Next.js Blog</h1>
+		<main className={styles.main}>
+			<h1 className={styles.title}>Next.js Blog</h1>
 
-				<p className={styles.description}>
-					A simple blog app made with the{' '}
-					<Link href="https://nextjs.org">
-						<a>Next.js</a>
-					</Link>{' '}
-					framework.
-				</p>
-				<br />
+			<p className={styles.description}>
+				A simple blog app made with the{' '}
+				<Link href="https://nextjs.org">
+					<a>Next.js</a>
+				</Link>{' '}
+				framework.
+			</p>
+			<br />
 
-				<p className={styles.description}>
-					<strong>
-						Read all posts{' '}
-						<Link href="/posts">
-							<a>here!</a>
-						</Link>
-					</strong>
-				</p>
+			<p className={styles.description}>
+				<strong>
+					Read all posts{' '}
+					<Link href="/posts">
+						<a>here!</a>
+					</Link>
+				</strong>
+			</p>
 
-				<div className={styles.postsContainer}>
-					<h2>Latest posts</h2>
+			<div className={styles.postsContainer}>
+				<h2>Latest posts</h2>
 
-					<div className={styles.posts}>
-						{recentPosts.length > 0 ? (
-							recentPosts
-								.map(post => (
-									<div className={styles.postItem} key={post._id}>
-										<Link href={`/posts/${post._id}`}>
-											<a>
-												<strong>{post.title}</strong>
-											</a>
-										</Link>
-										<br />
-										<DateTime timestamp={post.createdAt} />
-									</div>
-								))
-								.reduce<JSX.Element[]>(
-									(acc, post, i) =>
-										i === 0
-											? [post]
-											: [...acc, <hr key={'_' + post.key} />, post],
-									[]
-								)
-						) : (
-							<p className={styles.noPosts}>
-								Looks like there isn't any recent content!
-							</p>
-						)}
-					</div>
+				<div className={styles.posts}>
+					{recentPosts.length > 0 ? (
+						recentPosts
+							.map(post => (
+								<div className={styles.postItem} key={post._id}>
+									<Link href={`/posts/${post._id}`}>
+										<a>
+											<strong>{post.title}</strong>
+										</a>
+									</Link>
+									<br />
+									<DateTime timestamp={post.createdAt} />
+								</div>
+							))
+							.reduce<JSX.Element[]>(
+								(acc, post, i) =>
+									i === 0
+										? [post]
+										: [...acc, <hr key={'_' + post.key} />, post],
+								[]
+							)
+					) : (
+						<p className={styles.noPosts}>
+							Looks like there isn't any recent content!
+						</p>
+					)}
 				</div>
-			</main>
-		</div>
-	);
-};
+			</div>
+		</main>
+	</div>
+);
 
 export default Home;
