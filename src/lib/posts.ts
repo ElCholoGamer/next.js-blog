71fcb02d32031e2286e50e9af5b3d45702d92cb5
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-19 23:11:57 -0500

    fix getAllPosts function

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -3,8 +3,14 @@ import Post, { IPost } from '../models/post';
 
 export type PostInfo = ReturnType<IPost['toJSON']>;
 
+const formatPosts = (posts: IPost[]) =>
+	posts.map(post => post.toJSON()).sort((a, b) => b.createdAt - a.createdAt);
+
 export async function getAllPosts(): Promise<PostInfo[]> {
 	await db();
+
+	const posts = await Post.find();
+	return formatPosts(posts);
 }
 
 export async function getRecentPosts(days = 1): Promise<PostInfo[]> {
@@ -12,7 +18,5 @@ export async function getRecentPosts(days = 1): Promise<PostInfo[]> {
 	const offset = Date.now() - days * 24 * 60 * 60 * 1000;
 
 	const posts = await Post.find({ createdAt: { $gte: offset } });
-	return posts
-		.map(post => post.toJSON())
-		.sort((a, b) => b.createdAt - a.createdAt);
+	return formatPosts(posts);
 }
